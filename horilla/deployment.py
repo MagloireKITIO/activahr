071ab5d2b72d094f259f89e8bd8b68f01a6b1de2
Magloire Mr. KITIO <stage.dsi.pmo@group-activa.com>
2024-10-15 18:43:19 +0100

    candidate view erreur sur les urls

--- horilla/deployment.py
+++ horilla/deployment.py
@@ -77,3 +77,150 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "notifications",
+    "mathfilters",
+    "corsheaders",
+    "simple_history",
+    "django_filters",
+    "base",
+    "employee",
+    "recruitment",
+    "leave",
+    "pms",
+    "onboarding",
+    "asset",
+    "attendance",
+    "payroll",
+    "widget_tweaks",
+    "django_apscheduler",
+]
+APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
+
+APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
+
+ROOT_URLCONF = "horilla.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "horilla.wsgi.application"
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "oh-alert--warning",
+    messages.INFO: "oh-alert--info",
+    messages.SUCCESS: "oh-alert--success",
+    messages.WARNING: "oh-alert--warning",
+    messages.ERROR: "oh-alert--danger",
+}
+
+
+CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS")
+
+LOGIN_URL = "/login"
+
+
+SIMPLE_HISTORY_REVERT_DISABLED = True
+
+
+DJANGO_NOTIFICATIONS_CONFIG = {
+    "USE_JSONFIELD": True,
+    "SOFT_DELETE": True,
+    "USE_WATCHED": True,
+    "NOTIFICATIONS_STORAGE": "notifications.storage.DatabaseStorage",
+    "TEMPLATE": "notifications.html",  # Add this line
+}
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
+
+LANGUAGES = (
+    ("en", "English (US)"),
+    ("fr", "France"),
+)
+
+LOCALE_PATHS = [
+    join(BASE_DIR, "horilla", "locale"),
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Asia/Kolkata"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Production settings
+if not DEBUG:
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
