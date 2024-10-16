5918d64a0c49f3837ce83063db5d85789b72d8ba
Magloire KITIO <magloirekitio1@gmail.com>
2024-10-16 06:48:36 +0100

    modification deploy6

--- horilla/settings.py
+++ horilla/settings.py
@@ -113,8 +113,7 @@ WSGI_APPLICATION = "horilla.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+
 if env("DATABASE_URL", default=None):
     DATABASES = {
         "default": env.db(),
@@ -123,10 +122,11 @@ if env("DATABASE_URL", default=None):
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': 'db-recrutement',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
